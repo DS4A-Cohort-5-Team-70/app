59c9358a1010f975f3fe3b14017de5b1bc703c36
Gabriel Marquez <marquez122@outlook.com>
2021-07-29 06:57:13 -0500

    adds comments

--- app.py
+++ app.py
@@ -3,23 +3,29 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+#Envioromental variables
 import os
+from dotenv import load_dotenv
+#Dash
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
 from dash.dependencies import Input, Output
+#plotly
+import plotly.express as px
+#Pandas
 import pandas as pd
+#Postgres SQL
 import psycopg2 as pg
 import pandas.io.sql as psql
-from dotenv import load_dotenv
 
+#Creates dash app
 app = dash.Dash(__name__, title='Prediction Model')
 
+#Uses app's Flask server
 server = app.server
 
-#-------------------------------------------------------------------------------------
-#Data import from Postgres
+# Getting the data -------------------------------------------------------------------------------------
 
 #Loads env variables
 load_dotenv()
@@ -30,31 +36,32 @@ database = 'raw'
 
 #Creating the connection
 engine = pg.connect("host={} dbname={} user={} password={}".format(host, database, user, password))
+
 #Preparing the data
 df = psql.read_sql('SELECT * FROM asesor', engine)
+
 df.sort_values(by=['idfuncionario', 'fecha_retiro'], inplace=True)
 df_unique = df[df.duplicated(subset=['idfuncionario'], keep='last')]
 df_unique.reset_index(drop=True, inplace=True)
-colum_names = df_unique.columns
+column_names = df_unique.columns
 
 
-#--------------------------------------------------------------------------------------
-#App layout
+# App layout --------------------------------------------------------------------------------------
 
-app.layout = html.Div(children=[
+app.layout = html.Div([
     html.H1('Employee turnover',
             style={
                     'textAlign': 'center',
                 }
     ),
 
-    html.H4('Variable count by segmento'),
+    html.H4('Variable count by segmento'
+    ),
 
     dcc.Dropdown(
         id="x_value",
-        options=[{"label": x, "value": x} 
-                 for x in colum_names],
-        value=colum_names[10],
+        options=[{"label": column, "value": column} for column in column_names],
+        value=column_names[10],
         multi=False
     ),
 
@@ -63,15 +70,13 @@ app.layout = html.Div(children=[
     dcc.Graph(id="histogram")
 ])
 
+# Callbacks -------------------------------------------------------------------------------------------
+
 @app.callback(
     Output("histogram", "figure"), 
     Input("x_value", "value"))
 def update_chart(x_value):
     fig = px.histogram(df_unique, x=x_value, color="segmento")
-    """ fig.update_layout(
-        paper_bgcolor = '#b1c2e6',
-        plot_bgcolor = '#b1c2e6'
-    ) """
     return fig
 
 if __name__ == '__main__':
